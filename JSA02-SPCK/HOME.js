234486822e7ebf18a76d052397d14233ef3657d9
Minhquang11 <156317286+Minhquang11@users.noreply.github.com>
2024-03-31 09:53:16 +0700

    add

--- JSA02-SPCK/HOME.js
+++ JSA02-SPCK/HOME.js
@@ -1,53 +1,41 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+showSlides();
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("slide");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 2000); // Thay đổi hình sau mỗi 2 giây
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
 // Lấy phần tử input và button
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
 const searchResults = document.getElementById('searchResults');
 
 // Lắng nghe sự kiện click vào button
-searchButton.addEventListener('click', function() {
+searchButton.addEventListener('click', function () {
   search(searchInput.value);
 });
 
 // Lắng nghe sự kiện nhấn phím Enter trong input
-searchInput.addEventListener('keypress', function(event) {
+searchInput.addEventListener('keypress', function (event) {
   if (event.key === 'Enter') {
     search(searchInput.value);
   }
 });
-
+let item = ["Vợt Cầu Lông Lining Axforce Cannon - Trắng (Nội Địa Trung)", "Vợt Cầu Lông Kumpoo Power Control K520 Pro Trắng Hồng Chính Hãng", "Set Vợt Cầu Lông Kumpoo K520S - Hồng (Nội Địa Trung)", "Vợt Cầu Lông Yonex Nanoflare 800 Pro - Xách Tay",
+  "Vợt Cầu Lông Yonex Nanoflare 1000Z - Xách Tay", "Vợt Cầu Lông Yonex Nanoflare 700 Cyan - Xách Tay", "Vợt Cầu Lông Yonex Astrox 88D Pro 2024 Chính Hãng", "Vợt Cầu Lông Yonex Astrox 88S Pro 2024 Chính Hãng",
+"Vợt Cầu Lông Lining Halbertec 7000 (Nội Địa Trung)", "Vợt Cầu Lông Lining Halbertec 9000 (Nội Địa Trung)", "Vợt cầu lông Lining Axforce 80 Đỏ (Rồng Lửa) Chen Long Limited - Bản Quốc tế",
+"Vợt Cầu Lông Lining Aeronaut 9000C (Nội Địa Trung)"]
 // Hàm tìm kiếm
 function search(keyword) {
-  // Xử lý tìm kiếm ở đây, ví dụ:
-  // var searchResult = performSearch(keyword);
 
-  // Hiển thị kết quả tìm kiếm
-  searchResults.innerHTML = '<p>Kết quả tìm kiếm cho: ' + keyword + '</p>';
+    searchResults.innerHTML = '<p>Kết quả tìm kiếm cho: ' + keyword + '</p>';
 }
+
